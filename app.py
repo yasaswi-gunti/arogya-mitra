3f3adb80aaa6cf25d110f380b08b9ed7bd40a85f
yasaswi-gunti <guntiyasaswi@gmail.com>
2025-03-10 08:34:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ from ai import ask_ai
 def main():
     st.title("Arogya Mitra")
 
-    user_input = st.text_area("Enter your health-related query:")
+    user_profile = st.text_area("Enter your profile:", 
+                                value="30 year old male of height 180 cm and weight 70 kg")
+        
+    # Health-related query input
+    user_input = st.text_area("Enter your health-related query:", value="I'm having fever and headache from 3 days and vomits also")
 
     if st.button("Submit"):
         if user_input:
-            response = ask_ai(user_input)
+            response = ask_ai(user_profile, user_input)
             st.subheader("Response:")
             st.write(response)
         else:
